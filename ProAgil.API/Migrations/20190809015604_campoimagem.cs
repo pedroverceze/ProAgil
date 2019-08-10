428dc00fef243868e6bb58dd3d898864d2f2d675
PedroVerceze <pedroverceze@hotmail.com>
2019-08-10 09:18:32 -0300

    inicial meu projeto de eventos

---
+++ ProAgil.API/Migrations/20190809015604_campoimagem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.API.Migrations
+{
+    public partial class campoimagem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemURL",
+                table: "Eventos",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ImagemURL",
+                table: "Eventos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
